9c284ce3c617aa0640939ab4f169ea233319bd41
Thalia Cantero <thal.cantero@gmail.com>
2024-08-12 00:03:56 -0700

    Boggle version 1.0

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_board():
     highscore = session.get('highscore',0)
     nplays = session.get('nplays',0)
  
-    return render_template('board.html', board=board, highscore = highscore, nplays = nplays)
+    return render_template('index.html', board=board, highscore = highscore, nplays = nplays)
 
 @app.route('/check-word')
 def check_word():
